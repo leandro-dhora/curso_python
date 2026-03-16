0079ca227d11a814fd52bb3cfc5bdf827ca3023f
Leandro Da Hora <lsousa.dhora@gmail.com>
2026-03-16 13:32:12 -0300

    Add files via upload

---
+++ py_aula_5/identificacao.py
@@ -0,0 +1,10 @@
+n = input('Digite algo > ')
+
+print('O tipo primitivo desse valor e', type(n))
+print('So tem espacos?', n.isspace())
+print('E um numero?', n.isnumeric())
+print('E alfabetico?', n.isalpha())
+print('E alfanumerico?', n.isalnum())
+print('Esta em maiusculas?', n.isupper())
+print('Esta em minusculas?', n.islower())
+print('Esta capitalizada?', n.istitle())
